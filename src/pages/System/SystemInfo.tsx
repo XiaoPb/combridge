6c68b2eccb16f0c7b6fd89a599e7a165500e00a2
XiaoPb <lzh175603@163.com>
2026-04-06 12:56:34 +0800

    feat(ui): 系统页面信息TAB改为双列卡片布局

--- src/pages/System/SystemInfo.tsx
+++ src/pages/System/SystemInfo.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Descriptions, Progress, Tag, Spin, Typography, Button, Space, Divider } from 'antd';
+import { Card, Descriptions, Progress, Tag, Spin, Typography, Button, Space, Divider, Row, Col } from 'antd';
 import {
   DesktopOutlined,
   DatabaseOutlined,
@@ -123,150 +123,169 @@ const SystemInfo: React.FC = () => {
   }
 
   return (
-    <div style={{ display: 'grid', gap: 16, height: '100%', overflow: 'auto', paddingRight: 4 }}>
-      <Card
-        title={
-          <span>
-            <InfoCircleOutlined style={{ marginRight: 8 }} />
-            {t('title.about')}
-          </span>
-        }
-      >
-        <Descriptions column={2} size="small">
-          <Descriptions.Item label={t('label.appName')}>
-            <Text strong>ComBridge</Text>
-          </Descriptions.Item>
-          <Descriptions.Item label={t('label.version')}>
-            <Tag color="blue">v{appVersion || systemInfo?.app_version || '未知'}</Tag>
-          </Descriptions.Item>
-          <Descriptions.Item label={t('label.description')} span={2}>
-            {t('value.description')}
-          </Descriptions.Item>
-          <Descriptions.Item label={t('label.author')} span={2}>
-            ComBridge Team
-          </Descriptions.Item>
-        </Descriptions>
-        <Divider style={{ margin: '16px 0' }} />
-        <Space wrap>
-          <Button
-            type="link"
-            icon={<QuestionCircleOutlined />}
-            onClick={() => systemApi.openUrl('https://github.com/combridge/combridge/wiki')}
+    <div style={{ height: '100%', overflow: 'auto', paddingRight: 4 }}>
+      <Row gutter={[16, 16]}>
+        <Col span={12}>
+          <Card
+            size="small"
+            title={
+              <span>
+                <InfoCircleOutlined style={{ marginRight: 8 }} />
+                {t('title.about')}
+              </span>
+            }
           >
-            {t('button.documentation')}
-          </Button>
-          <Button
-            type="link"
-            icon={<GithubOutlined />}
-            onClick={() => systemApi.openUrl('https://github.com/combridge/combridge')}
+            <Descriptions column={1} size="small">
+              <Descriptions.Item label={t('label.appName')}>
+                <Text strong>ComBridge</Text>
+              </Descriptions.Item>
+              <Descriptions.Item label={t('label.version')}>
+                <Tag color="blue">v{appVersion || systemInfo?.app_version || '未知'}</Tag>
+              </Descriptions.Item>
+              <Descriptions.Item label={t('label.description')}>
+                {t('value.description')}
+              </Descriptions.Item>
+              <Descriptions.Item label={t('label.author')}>
+                ComBridge Team
+              </Descriptions.Item>
+            </Descriptions>
+            <Divider style={{ margin: '12px 0' }} />
+            <Space wrap size="small">
+              <Button
+                type="link"
+                size="small"
+                icon={<QuestionCircleOutlined />}
+                onClick={() => systemApi.openUrl('https://github.com/combridge/combridge/wiki')}
+              >
+                {t('button.documentation')}
+              </Button>
+              <Button
+                type="link"
+                size="small"
+                icon={<GithubOutlined />}
+                onClick={() => systemApi.openUrl('https://github.com/combridge/combridge')}
+              >
+                {t('button.github')}
+              </Button>
+              <Button
+                type="link"
+                size="small"
+                icon={<BugOutlined />}
+                onClick={() => systemApi.openUrl('https://github.com/combridge/combridge/issues')}
+              >
+                {t('button.feedback')}
+              </Button>
+            </Space>
+          </Card>
+        </Col>
+
+        <Col span={12}>
+          <Card
+            size="small"
+            title={
+              <span>
+                <DesktopOutlined style={{ marginRight: 8 }} />
+                {t('title.systemInfo')}
+              </span>
+            }
           >
-            {t('button.github')}
-          </Button>
-          <Button
-            type="link"
-            icon={<BugOutlined />}
-            onClick={() => systemApi.openUrl('https://github.com/combridge/combridge/issues')}
+            {systemInfo && (
+              <Descriptions column={1} size="small">
+                <Descriptions.Item label={t('label.os')}>{systemInfo.os_name}</Descriptions.Item>
+                <Descriptions.Item label={t('label.osVersion')}>{systemInfo.os_version}</Descriptions.Item>
+                <Descriptions.Item label={t('label.arch')}>{systemInfo.arch}</Descriptions.Item>
+                <Descriptions.Item label={t('label.hostname')}>{systemInfo.hostname}</Descriptions.Item>
+                <Descriptions.Item label={t('label.cpuCores')}>{systemInfo.cpu_count} {t('value.cores')}</Descriptions.Item>
+                <Descriptions.Item label={t('label.totalMemory')}>
+                  {formatBytes(systemInfo.total_memory)}
+                </Descriptions.Item>
+                <Descriptions.Item label={t('label.appVersion')}>
+                  <Tag color="blue">v{systemInfo.app_version}</Tag>
+                </Descriptions.Item>
+              </Descriptions>
+            )}
+          </Card>
+        </Col>
+
+        <Col span={12}>
+          <Card
+            size="small"
+            title={
+              <span>
+                <CloudServerOutlined style={{ marginRight: 8 }} />
+                {t('title.runtimeStatus')}
+              </span>
+            }
           >
-            {t('button.feedback')}
-          </Button>
-        </Space>
-      </Card>
+            {systemStatus && (
+              <div>
+                <div style={{ marginBottom: 16 }}>
+                  <Text strong>{t('label.cpuUsage')}</Text>
+                  <Progress
+                    percent={Math.round(systemStatus.cpu_usage)}
+                    size="small"
+                    status={systemStatus.cpu_usage > 80 ? 'exception' : 'active'}
+                    strokeColor={{
+                      '0%': '#108ee9',
+                      '100%': systemStatus.cpu_usage > 80 ? '#ff4d4f' : '#87d068',
+                    }}
+                  />
+                </div>
 
-      <Card
-        title={
-          <span>
-            <DesktopOutlined style={{ marginRight: 8 }} />
-            {t('title.systemInfo')}
-          </span>
-        }
-      >
-        {systemInfo && (
-          <Descriptions column={2} bordered size="small">
-            <Descriptions.Item label={t('label.os')}>{systemInfo.os_name}</Descriptions.Item>
-            <Descriptions.Item label={t('label.osVersion')}>{systemInfo.os_version}</Descriptions.Item>
-            <Descriptions.Item label={t('label.arch')}>{systemInfo.arch}</Descriptions.Item>
-            <Descriptions.Item label={t('label.hostname')}>{systemInfo.hostname}</Descriptions.Item>
-            <Descriptions.Item label={t('label.cpuCores')}>{systemInfo.cpu_count} {t('value.cores')}</Descriptions.Item>
-            <Descriptions.Item label={t('label.totalMemory')}>
-              {formatBytes(systemInfo.total_memory)}
-            </Descriptions.Item>
-            <Descriptions.Item label={t('label.appVersion')}>
-              <Tag color="blue">v{systemInfo.app_version}</Tag>
-            </Descriptions.Item>
-          </Descriptions>
-        )}
-      </Card>
+                <div style={{ marginBottom: 16 }}>
+                  <Text strong>
+                    {t('label.memoryUsage')} ({formatBytes(systemStatus.used_memory)} /{' '}
+                    {formatBytes(systemStatus.total_memory)})
+                  </Text>
+                  <Progress
+                    percent={Math.round(systemStatus.memory_usage)}
+                    size="small"
+                    status={systemStatus.memory_usage > 80 ? 'exception' : 'active'}
+                    strokeColor={{
+                      '0%': '#108ee9',
+                      '100%': systemStatus.memory_usage > 80 ? '#ff4d4f' : '#87d068',
+                    }}
+                  />
+                </div>
 
-      <Card
-        title={
-          <span>
-            <CloudServerOutlined style={{ marginRight: 8 }} />
-            {t('title.runtimeStatus')}
-          </span>
-        }
-      >
-        {systemStatus && (
-          <div>
-            <div style={{ marginBottom: 24 }}>
-              <Text strong>{t('label.cpuUsage')}</Text>
-              <Progress
-                percent={Math.round(systemStatus.cpu_usage)}
-                status={systemStatus.cpu_usage > 80 ? 'exception' : 'active'}
-                strokeColor={{
-                  '0%': '#108ee9',
-                  '100%': systemStatus.cpu_usage > 80 ? '#ff4d4f' : '#87d068',
-                }}
-              />
-            </div>
+                <div>
+                  <Text strong>
+                    <ClockCircleOutlined style={{ marginRight: 8 }} />
+                    {t('label.systemUptime')}
+                  </Text>
+                  <Text style={{ marginLeft: 8 }}>{formatUptime(systemStatus.uptime_secs, t)}</Text>
+                </div>
+              </div>
+            )}
+          </Card>
+        </Col>
 
-            <div style={{ marginBottom: 24 }}>
-              <Text strong>
-                {t('label.memoryUsage')} ({formatBytes(systemStatus.used_memory)} /{' '}
-                {formatBytes(systemStatus.total_memory)})
-              </Text>
-              <Progress
-                percent={Math.round(systemStatus.memory_usage)}
-                status={systemStatus.memory_usage > 80 ? 'exception' : 'active'}
-                strokeColor={{
-                  '0%': '#108ee9',
-                  '100%': systemStatus.memory_usage > 80 ? '#ff4d4f' : '#87d068',
-                }}
-              />
-            </div>
-
-            <div style={{ marginBottom: 16 }}>
-              <Text strong>
-                <ClockCircleOutlined style={{ marginRight: 8 }} />
-                {t('label.systemUptime')}
-              </Text>
-              <Text style={{ marginLeft: 8 }}>{formatUptime(systemStatus.uptime_secs, t)}</Text>
-            </div>
-          </div>
-        )}
-      </Card>
-
-      <Card
-        title={
-          <span>
-            <DatabaseOutlined style={{ marginRight: 8 }} />
-            {t('title.diskUsage')}
-          </span>
-        }
-      >
-        {systemStatus?.disk_usage.map((disk, index) => (
-          <div key={index} style={{ marginBottom: 16 }}>
-            <Text strong>{disk.name}</Text>
-            <Text type="secondary" style={{ marginLeft: 8 }}>
-              ({formatBytes(disk.used_space)} / {formatBytes(disk.total_space)})
-            </Text>
-            <Progress
-              percent={Math.round(disk.usage_percent)}
-              size="small"
-              status={disk.usage_percent > 90 ? 'exception' : 'active'}
-            />
-          </div>
-        ))}
-      </Card>
+        <Col span={12}>
+          <Card
+            size="small"
+            title={
+              <span>
+                <DatabaseOutlined style={{ marginRight: 8 }} />
+                {t('title.diskUsage')}
+              </span>
+            }
+          >
+            {systemStatus?.disk_usage.map((disk, index) => (
+              <div key={index} style={{ marginBottom: index < systemStatus.disk_usage.length - 1 ? 12 : 0 }}>
+                <Text strong>{disk.name}</Text>
+                <Text type="secondary" style={{ marginLeft: 8 }}>
+                  ({formatBytes(disk.used_space)} / {formatBytes(disk.total_space)})
+                </Text>
+                <Progress
+                  percent={Math.round(disk.usage_percent)}
+                  size="small"
+                  status={disk.usage_percent > 90 ? 'exception' : 'active'}
+                />
+              </div>
+            ))}
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
